3345bf2af463b1f24a2b0dc1251055ad128a84c4
Rassina10 <rassinatayerbanga@gmail.com>
2025-11-06 17:22:25 +0100

    Initialisation

--- front/src/components/data-table-schema.tsx
+++
@@ -1,9 +0,0 @@
-export const schema = z.object({
-  id: z.number(),
-  header: z.string(),
-  type: z.string(),
-  status: z.string(),
-  target: z.string(),
-  limit: z.string(),
-  reviewer: z.string(),
-})
